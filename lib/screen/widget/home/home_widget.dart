c944798d60d8981030d667c2b36c2be863c07192
nalaadina <inter.dev@nexagroup.co.id>
2024-04-09 22:51:15 +0700

    Polish some little detail to APK

--- lib/screen/widget/home/home_widget.dart
+++ lib/screen/widget/home/home_widget.dart
@@ -278,10 +278,9 @@ class _TopPortion extends StatelessWidget {
                 Container(
                   decoration: const BoxDecoration(
                     shape: BoxShape.circle,
-                    image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(
-                            'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')),
+                    image: DecorationImage(fit: BoxFit.cover, image: NetworkImage(
+                        // 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+                        'https://cdn-icons-png.flaticon.com/512/4140/4140037.png')),
                   ),
                   child: Visibility(
                     // Hide the black circle when image is visible
